d5cb9ed9677b688f8163c3665d688f4597903096
RHamudi <ramonramos.silva16@gmail.com>
2025-07-16 10:24:02 -0300

    feat: create page home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
+        loadComponent: () => import('./features/home/home').then(m => m.Home)
+    },
+    {
+        path: 'login',
         loadComponent: () => import('./features/login/login').then(m => m.Login)
     },
     {
-        path: 'cadastro',
+        path: 'register',
         loadComponent: () => import('./features/register-user/register-user').then(m => m.RegisterUser)
     }
 ];

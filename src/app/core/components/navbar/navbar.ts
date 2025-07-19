601d79c7e99cb191619d3037af350cf3c08a4dde
RHamudi <ramonramos.silva16@gmail.com>
2025-07-19 12:23:57 -0300

    feat: navbar

--- src/app/core/components/navbar/navbar.ts
+++ src/app/core/components/navbar/navbar.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  route: string = '';
+  constructor(private router: Router) { 
+    this.route = this.router.url.split('/')[1] || '';
+  }
 
+  navigateToHome() {
+    this.router.navigate(['']);
+  }
+  // Add any methods or properties needed for the navbar functionality
 }
